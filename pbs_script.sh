f1d7048467a1828e75c1fce3913e580b5e261874
Geokats <geo.katsogiannis@gmail.com>
2019-09-29 12:39:09 +0300

    Fix script

--- pbs_script.sh
+++ pbs_script.sh
@@ -7,12 +7,12 @@
 #PBS -l walltime=0:01:00  		# Example, 1 minute
 
 # How many nodes and tasks per node
-PBS -l nodes=2:ppn=8  			# 2 nodes with 8 tasks/node => 16 tasks
+#PBS -l nodes=2:ppn=8  			# 2 nodes with 8 tasks/node => 16 tasks
 
 #Which Queue
-PBS -q parsys			 	# This is the only accessible queue for rbs
+#PBS -q parsys			 	# This is the only accessible queue for rbs
 
-PBS -N heat2Dn   			# Jobname - it gives the stdout/err filenames
+#PBS -N heat2Dn   			# Jobname - it gives the stdout/err filenames
 
 ### Merge std[out|err]
 #PBS -k oe
